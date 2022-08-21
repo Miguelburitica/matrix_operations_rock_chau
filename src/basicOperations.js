2d559c03d50a0ba7bf95b65c5c87cdac738914a1
juan-alvarz <juanalvarez0925@gmail.com>
2022-08-21 13:59:52 -0500

    change some things in test

--- src/basicOperations.js
+++ src/basicOperations.js
@@ -1,38 +1,38 @@
-const utils = require('./utils')
+const utils = require("./utils");
 
 exports.addition = function (matrixA, matrixB) {
-  const matrixC = utils.zerosmatrix(matrixA.length)
+  const matrixC = utils.zerosMatriz(matrixA.length);
   for (let i = 0; i < matrixA.length; i++) {
     for (let j = 0; j < matrixB.length; j++) {
-      matrixC[i][j] = matrixA[i][j] + matrixB[i][j]
+      matrixC[i][j] = matrixA[i][j] + matrixB[i][j];
     }
   }
-  return matrixC
-}
+  return matrixC;
+};
 
 exports.substraction = function (matrixA, matrixB) {
-  const matrixC = utils.zerosmatrix(matrixA.length)
+  const matrixC = utils.zerosmatrix(matrixA.length);
   for (let i = 0; i < matrixA.length; i++) {
     for (let j = 0; j < matrixB.length; j++) {
-      matrixC[i][j] = matrixA[i][j] + matrixB[i][j]
+      matrixC[i][j] = matrixA[i][j] + matrixB[i][j];
     }
   }
-  return matrixC
-}
+  return matrixC;
+};
 
 exports.multiplication = function (matrixA, matrixB, m, n = m, p = m) {
-  const matrixC = []
-  let matrixpC = []
+  const matrixC = [];
+  let matrixpC = [];
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      let placeHolderMatrixC = 0
+      let placeHolderMatrixC = 0;
       for (let k = 0; k < p; k++) {
-        placeHolderMatrixC += matrixA[i][k] * matrixB[k][j]
+        placeHolderMatrixC += matrixA[i][k] * matrixB[k][j];
       }
-      matrixpC.push(placeHolderMatrixC)
+      matrixpC.push(placeHolderMatrixC);
     }
-    matrixC.push(matrixpC)
-    matrixpC = []
+    matrixC.push(matrixpC);
+    matrixpC = [];
   }
-  return matrixC
-}
+  return matrixC;
+};
--- test/basicOperations/addition.test.js
+++ test/basicOperations/addition.test.js
@@ -1,10 +1,30 @@
-const basicOperations = require('../../src/basicOperations')
+const basicOperations = require("../../src/basicOperations");
 
-test('add a matrix 2x2 to another matrix 2x2 with just 1s', () => {
+describe("addition operation test", () => {
+  test("add a matrix 2x2 to another matrix 2x2 with just 1s", () => {
+    expect(
+      basicOperations.addition(
+        [
+          [1, 1],
+          [1, 1],
+        ],
+        [
+          [1, 1],
+          [1, 1],
+        ]
+      )
+    ).toBe([
+      [2, 2],
+      [2, 2],
+    ]);
+  });
+});
+
+/* test('add a matrix 2x2 to another matrix 2x2 with just 1s', () => {
   expect(
     basicOperations.addition(
       [[1, 1], [1, 1]],
       [[1, 1], [1, 1]]
     )
   ).toBe([[2, 2], [2, 2]])
-})
+}) */
